4b152ee9c100bdc5c0f27c0211c9a73ebdf29c11
MarcioPortela <marcio.portela@fatec.sp.gov.br>
2026-03-25 21:12:30 -0300

    Add test projects to Domain layer and LowRiskRule unit tests

---
+++ TradeRiskEvaluator.Domain.Tests/RiskRules/LowRiskRuleTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using TradeRiskEvaluator.Domain.Entities;
+using TradeRiskEvaluator.Domain.Enum;
+using TradeRiskEvaluator.Domain.RiskRules;
+
+namespace TradeRiskEvaluator.Domain.Tests.RiskRules
+{
+    public class LowRiskRuleTests
+    {
+        private readonly LowRiskRule _rule;
+
+        public LowRiskRuleTests()
+        {
+            _rule = new LowRiskRule();
+        }
+
+        [Fact]
+        public void Category_ShouldReturn_LOWRISK()
+        {
+            var category = _rule.Category;
+
+            Assert.Equal("LOWRISK", category);
+        }
+
+        [Theory]
+        [InlineData(0, true)]
+        [InlineData(500000, true)]
+        [InlineData(999999.99, true)]
+        [InlineData(1000000, false)]
+        [InlineData(2000000, false)]
+        public void IsMatch_ShouldEvaluateCorrectly_BasedOnTradeValue(double tradeValueDouble, bool expected)
+        {
+            decimal tradeValue = (decimal)tradeValueDouble;
+            var trade = new Trade(tradeValue, Sector.Private);
+
+            var result = _rule.IsMatch(trade);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
