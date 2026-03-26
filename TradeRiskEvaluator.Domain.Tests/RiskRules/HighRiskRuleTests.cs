5d5e2de32a4185e4775cb1e5303e6b685b75686a
MarcioPortela <marcio.portela@fatec.sp.gov.br>
2026-03-25 21:53:42 -0300

    Add unit tests for HighRiskRule and MediumRiskRule

---
+++ TradeRiskEvaluator.Domain.Tests/RiskRules/HighRiskRuleTests.cs
@@ -0,0 +1,46 @@
+﻿using TradeRiskEvaluator.Domain.Entities;
+using TradeRiskEvaluator.Domain.Enum;
+using TradeRiskEvaluator.Domain.RiskRules;
+
+namespace TradeRiskEvaluator.Domain.Tests.RiskRules
+{
+    public class HighRiskRuleTests
+    {
+        private readonly HighRiskRule _rule;
+
+        public HighRiskRuleTests()
+        {
+            _rule = new HighRiskRule();
+        }
+
+        [Fact]
+        public void Category_ShouldReturn_HIGHRISK()
+        {
+            var category = _rule.Category;
+
+            Assert.Equal("HIGHRISK", category);
+        }
+
+        [Theory]
+        [InlineData(2000000, Sector.Private, true)]
+        [InlineData(1000000.01, Sector.Private, true)]
+
+        [InlineData(1000000, Sector.Private, false)]
+        [InlineData(999999.99, Sector.Private, false)]
+        [InlineData(500000, Sector.Private, false)]
+
+        [InlineData(2000000, Sector.Public, false)]
+        [InlineData(1000000.01, Sector.Public, false)]
+        [InlineData(500000, Sector.Public, false)]
+        public void IsMatch_ShouldEvaluateCorrectly_BasedOnTradeValueAndSector(double tradeValueDouble, Sector clientSector, bool expected)
+        {
+            decimal tradeValue = (decimal)tradeValueDouble;
+            var trade = new Trade(tradeValue, clientSector);
+
+            var result = _rule.IsMatch(trade);
+
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
---
+++ TradeRiskEvaluator.Domain.Tests/RiskRules/MediumRiskRuleTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using TradeRiskEvaluator.Domain.Entities;
+using TradeRiskEvaluator.Domain.Enum;
+using TradeRiskEvaluator.Domain.RiskRules;
+
+namespace TradeRiskEvaluator.Domain.Tests.RiskRules
+{
+    public class MediumRiskRuleTests
+    {
+        private readonly MediumRiskRule _rule;
+
+        public MediumRiskRuleTests()
+        {
+            _rule = new MediumRiskRule();
+        }
+
+        [Fact]
+        public void Category_ShouldReturn_MEDIUMRISK()
+        {
+            var category = _rule.Category;
+
+            Assert.Equal("MEDIUMRISK", category);
+        }
+
+        [Theory]
+        [InlineData(2000000, Sector.Public, true)]
+        [InlineData(1000000.01, Sector.Public, true)]
+
+        [InlineData(1000000, Sector.Public, false)]
+        [InlineData(999999.99, Sector.Public, false)]
+        [InlineData(500000, Sector.Public, false)]
+
+        [InlineData(2000000, Sector.Private, false)]
+        [InlineData(1000000.01, Sector.Private, false)]
+        [InlineData(500000, Sector.Private, false)]
+        public void IsMatch_ShouldEvaluateCorrectly_BasedOnTradeValueAndSector(double tradeValueDouble, Sector clientSector, bool expected)
+        {
+            decimal tradeValue = (decimal)tradeValueDouble;
+            var trade = new Trade(tradeValue, clientSector);
+
+            var result = _rule.IsMatch(trade);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
